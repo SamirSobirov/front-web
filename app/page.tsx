1652e758fc506d10c6afbf2661ac49e0e82d4199
Sobirov S <sobiroff23@gmail.com>
2024-08-03 02:31:13 +0500

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HomePage from './pages/Home';
+import Services from './pages/Services';
 
 
 const  Page: React.FC = () => {
   return (
     <>
     <HomePage></HomePage>
-
+    <Services></Services>
     </>
 
   );
