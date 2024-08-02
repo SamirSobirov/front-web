7757fb90b2f79bac8ae3b6ff12c456abcacbf64d
Sobirov S <sobiroff23@gmail.com>
2024-08-02 16:59:50 +0500

    fixed

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <header>
+		<div className="header__wrapper  ui-container"> <a className="header__logo" href="https://skillbox.uz/ru">
+				<h1 className="whitespace-nowrap font-bold font-3xl text-3xl"> MAPS AGENCY</h1>
+
+			</a> </div>
+	</header>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+    </>
+  );
+}
